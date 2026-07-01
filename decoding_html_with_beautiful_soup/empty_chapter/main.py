87f49e04755c9df27fd58f7ee5c4ffa6fdc84d9f
sprit-maberasu-1140-source <>
2026-07-01 05:22:37 +0000

    Empty chapter

---
+++ decoding_html_with_beautiful_soup/empty_chapter/main.py
@@ -0,0 +1,18 @@
+# Import libraries
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+# Open the page
+url = "https://codefinity-content-media.s3.eu-west-1.amazonaws.com/18a4e428-1a0f-44c2-a8ad-244cd9c7985e/jesus.html"
+page = urlopen(url)
+html = page.read().decode("utf-8")
+
+# Create the BeautifulSoup object
+soup = BeautifulSoup(html, "html.parser")
+
+# Save the list with all the `<p>` elements
+p = soup.find_all('p')
+print(p)
+
+# Output number of `<p>` elements
+print(len(p))
