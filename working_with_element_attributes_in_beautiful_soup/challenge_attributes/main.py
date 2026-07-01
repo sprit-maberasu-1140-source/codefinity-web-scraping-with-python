8930cb5504d5aee08d5fe2b62fc75dd9a299d55a
sprit-maberasu-1140-source <>
2026-07-01 06:09:31 +0000

    Challenge: Attributes

---
+++ working_with_element_attributes_in_beautiful_soup/challenge_attributes/main.py
@@ -0,0 +1,17 @@
+# Import libraries
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+# Open the page
+url = "https://codefinity-content-media.s3.eu-west-1.amazonaws.com/18a4e428-1a0f-44c2-a8ad-244cd9c7985e/jesus.html"
+page = urlopen(url)
+html = page.read().decode("utf-8")
+
+# Create the BeautifulSoup object
+soup = BeautifulSoup(html, "html.parser")
+
+# Print attributes of the p tag
+print(soup.p.attrs)
+
+# Print only the text of the ul tag
+print(soup.ul.get_text())
