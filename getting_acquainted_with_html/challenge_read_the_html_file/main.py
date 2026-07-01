1067ba90c2c5bd117135d760f2308dace1a27a7b
sprit-maberasu-1140-source <>
2026-07-01 04:32:19 +0000

    Challenge: Read the HTML File

---
+++ getting_acquainted_with_html/challenge_read_the_html_file/main.py
@@ -0,0 +1,10 @@
+# Importing the module
+from urllib.request import urlopen
+
+# Opening web page
+url = "https://codefinity-content-media.s3.eu-west-1.amazonaws.com/18a4e428-1a0f-44c2-a8ad-244cd9c7985e/page.html"
+page = urlopen(url)
+
+# Reading and decoding
+web_page = page.read().decode("utf-8")
+print(web_page)
